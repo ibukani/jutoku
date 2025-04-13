2c22e93d17058ae05fd3763e358362b2e4d0aa85
ibukani <ibukani2056@gmail.com>
2025-04-13 19:45:11 +0900

    きれいにする

--- src-tauri/src/systemtray.rs
+++ src-tauri/src/systemtray.rs
@@ -1,13 +1,18 @@
 use tauri::{
-    menu::{Menu, MenuItem},
+    menu::{Menu, MenuItem, PredefinedMenuItem},
     App, Manager,
 };
 
+use crate::feature::ClockWindow;
+
 pub fn init_systemtray(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     // システムトレイのアイコンを作成
+    let separator = PredefinedMenuItem::separator(app)?;
     let quit_icon = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
     let clock_icon = MenuItem::with_id(app, "clock", "Clock", true, None::<&str>)?;
-    let menu = Menu::with_items(app, &[&quit_icon, &clock_icon])?;
+    let menu = Menu::with_items(app, &[&clock_icon, &separator, &quit_icon])?;
+
+    // Trayのビルド
     let _tray = tauri::tray::TrayIconBuilder::new()
         .menu(&menu)
         .icon(app.default_window_icon().unwrap().clone())
@@ -16,15 +21,7 @@ pub fn init_systemtray(app: &mut App) -> Result<(), Box<dyn std::error::Error>> 
                 app.exit(0);
             }
             "clock" => {
-                // ウィンドウの作成
-                let window = app.get_webview_window("clock").unwrap();
-
-                if window.is_visible().unwrap() {
-                    window.hide().unwrap();
-                } else {
-                    crate::move_window_top_right(&window);
-                    window.show().unwrap();
-                }
+                ClockWindow::run(app);
             }
             _ => {}
         })
